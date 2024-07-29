8189227fcbf7a1efd145dc5a23216f5ac31d0a70
Thiemo Ottersbach <thiemo.ott@gmail.com>
2024-07-29 15:03:43 +0200

    functioning prottype

--- scdl_serato_fixed.py
+++ scdl_serato_fixed.py
@@ -1,17 +1,23 @@
 import glob
+import logging
 import os
 import subprocess
+import sys
+import traceback
+from logging import info
 from pathlib import Path
 
-from ffmpeg import FFmpeg, Progress
+from ffmpeg import FFmpeg
 from mutagen.flac import FLAC
 from mutagen.mp4 import MP4
 from send2trash import send2trash
 
 SILENCE_FILE_NAME = 'silence.m4a'
 SILENCE_DURATION = 0.01
-ARCHIVE_PATH = '.\\Extended Mixes\\archive.txt'
+CONCAT_TXT_FILE_NAME = 'concat.txt'
+ARCHIVE_PATH = r'.\Extended Mixes\archive.txt'
 BLANK = ''
+URL = 'https://soundcloud.com/tjerbor-fritzasnt/sets/dll'
 
 
 def render_silence():
@@ -21,10 +27,6 @@ def render_silence():
         .input('anullsrc=channel_layout=stereo:sample_rate=48000') \
         .option('t', SILENCE_DURATION) \
         .output(SILENCE_FILE_NAME)
-
-    @ffmpeg.on("progress")
-    def on_progress(progress: Progress):
-        print(progress)
 
     ffmpeg.execute()
 
@@ -38,7 +40,7 @@ def delete_silence():
 
 
 def concant_silence(original_filepath: str, muxed_filedpath: str):
-    with open('concat.txt', 'w', encoding="utf-8") as txt:
+    with open(CONCAT_TXT_FILE_NAME, 'w', encoding="utf-8") as txt:
         txt.write(
             f'file \'{SILENCE_FILE_NAME}\'\nfile \'{original_filepath}\''
         )
@@ -47,16 +49,8 @@ def concant_silence(original_filepath: str, muxed_filedpath: str):
         .option('y') \
         .option('f', 'concat') \
         .option('safe', 0) \
-        .input(SILENCE_FILE_NAME) \
-        .option('write_id3v2', 1) \
-        .option('write_apetag', 1) \
-        .option('write_mpeg2', 1) \
-        .option('map_metadata', 1) \
-        .output(muxed_filedpath, codec="copy")
-
-    @ffmpeg.on("progress")
-    def on_progress(progress: Progress):
-        print(progress)
+        .input(CONCAT_TXT_FILE_NAME) \
+        .output(muxed_filedpath, c='copy')
 
     ffmpeg.execute()
 
@@ -64,37 +58,41 @@ def concant_silence(original_filepath: str, muxed_filedpath: str):
 def fix_m4a_files(files):
     render_silence()
 
-    for file in files:
-        muxed_filepath = f'{Path(file).with_suffix(BLANK)}-copy.m4a'
-        concant_silence(file, muxed_filepath)
+    try:
+        for file in files:
+            muxed_filepath = f'{Path(file).with_suffix(BLANK)}-copy.m4a'
+            concant_silence(file, muxed_filepath)
 
-        original_m4a = MP4(file)
-        muxed_m4a = MP4(muxed_filepath)
-        original_m4a.tags = muxed_m4a.tags
-        original_m4a.save()
+            original_m4a = MP4(file)
+            muxed_m4a = MP4(muxed_filepath)
+            muxed_m4a.tags = original_m4a.tags
+            muxed_m4a.save()
 
-        send2trash(file)
+            send2trash(file)
+            os.rename(muxed_filepath, file)
+    except Exception:
+        print(traceback.format_exc())
 
+    Path.unlink(Path(CONCAT_TXT_FILE_NAME), missing_ok=True)
     delete_silence()
 
 
-def download(URL: str):
-    subprocess.run(['scdl.exe', '-l', str(URL), '--force-metadata', '--original-name', '--auth-token', '%authtoken%',
-                    '--no-playlist-folder', '--playlist-name-format', '{title}', '--download-archive', ARCHIVE_PATH])
-
-    raise NotImplementedError
+def download(url: str):
+    subprocess.run(['scdl.exe', '-l', str(url), '--force-metadata', '--original-name', '--auth-token',
+                    os.environ.get('authtoken', ''),
+                    '--no-playlist-folder', '--playlist-name-format', r'{title}', '--download-archive', ARCHIVE_PATH])
 
 
 def clean_archive(filepath):
-    with open(ARCHIVE_PATH, 'r') as archive:
+    with open(filepath, 'r') as archive:
         IDs = sorted(set(archive.read().splitlines()))
 
-    with open(ARCHIVE_PATH, 'w') as archive:
+    with open(filepath, 'w') as archive:
         archive.write('\n'.join(IDs))
 
 
 def downscale_flac():
-    flacs = glob.glob('*.flac', recursive=False)
+    flacs = glob.glob('*.flac')
     for flac in flacs:
         audio = FLAC(flac)
         if audio.info.bits_per_sample == 24:
@@ -115,18 +113,14 @@ def downscale_flac():
                          'ar': sample_rate}
                         )
 
-            @ffmpeg.on("progress")
-            def on_progress(progress: Progress):
-                print(progress)
-
             ffmpeg.execute()
 
             send2trash(flac)
             os.rename(output_filepath, flac)
 
 
-def convert_flac_to_wav():
-    wavs = glob.glob('*.wav', recursive=False)
+def convert_wav_to_flac():
+    wavs = glob.glob('*.wav')
     for wav in wavs:
         flac_filepath = f'{Path(wav).with_suffix(BLANK)}.flac'
         ffmpeg = FFmpeg() \
@@ -136,21 +130,30 @@ def convert_flac_to_wav():
                     {'acodec': 'flac',
                      'sample_fmt': 's16'}
                     )
+
+        ffmpeg.execute()
         send2trash(wav)
 
 
-def main(URL: str):
-    m4a_files_before_download = set(glob.glob('*.m4a', recursive=False))
-    download(URL)
+def main(url: str):
+    # Files to ignore
+    m4a_files_before_download = set(glob.glob('*.m4a'))
+
+    info(f'Downloading playlist entries from {url}')
+    download(url)
+    info('Cleaning archive.')
     clean_archive(ARCHIVE_PATH)
-    m4a_files_after_download = set(glob.glob('*.m4a', recursive=False))
 
-    downscale_flac()
-    convert_flac_to_wav()
-
+    info('Fixing m4a files for Serato.')
+    m4a_files_after_download = set(glob.glob('*.m4a'))
     fix_m4a_files(list(m4a_files_after_download - m4a_files_before_download))
+
+    info('Downscaling flac files.')
+    downscale_flac()
+    info('Converting wav files to flac.')
+    convert_wav_to_flac()
 
 
 if __name__ == '__main__':
-    # main(pyperclip.paste())
-    download('s')
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='INFO: %(message)s')
+    main(URL)

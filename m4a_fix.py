7127b57d3e386ca78fc3090f204a3179c47775e5
Thiemo Ottersbach <thiemo.ott@gmail.com>
2025-04-27 16:55:43 +0200

    feat(m4a_fix): Add different silence render samplerates

--- m4a_fix.py
+++ m4a_fix.py
@@ -11,35 +11,41 @@ from ffmpeg import FFmpeg
 from mutagen.mp4 import MP4
 from send2trash import send2trash
 
-SILENCE_FILE_NAME = '__silence__.m4a'
+SILENCE_FILE_NAME = '__silence__'
 SILENCE_DURATION = 0.01
 CONCAT_TXT_FILE_NAME = 'concat.txt'
 
 
-def render_silence(silence_file_name: str = SILENCE_FILE_NAME, silence_duration: int | float = SILENCE_DURATION):
+def render_silence(sample_rate: int, silence_file_name_prefix: str = SILENCE_FILE_NAME,
+                   silence_duration: int | float = SILENCE_DURATION) -> str:
+    output_path = f'{silence_file_name_prefix}{sample_rate}.m4a'
+
     ffmpeg = FFmpeg() \
         .option('y') \
         .option('f', 'lavfi') \
-        .input('anullsrc=channel_layout=stereo:sample_rate=48000') \
+        .input(f'anullsrc=channel_layout=stereo:sample_rate={sample_rate}') \
         .option('t', silence_duration) \
-        .output(silence_file_name)
+        .output(output_path)
 
     ffmpeg.execute()
 
-
-def delete_silence(silence_file_name: str):
-    if os.path.isfile(silence_file_name):
-        os.remove(silence_file_name)
-    else:
-        # If it fails, inform the user.
-        print(f'Error: {silence_file_name} does not exist.')
+    return output_path
 
 
-def concat_silence(audio_file_path: str, muxed_audio_file_path: str):
+def delete_silences(silences: dict):
+    for key in silences.keys():
+        if os.path.isfile(silences[key]):
+            os.remove(silences[key])
+        else:
+            # If it fails, inform the user.
+            print(f'Error: {silences[key]} does not exist.')
+
+
+def concat_silence(audio_file_path: str, muxed_audio_file_path: str, silence_filepath: str):
     def create_concat_txt(input_audio_file_path: str):
         with open(CONCAT_TXT_FILE_NAME, 'w', encoding="utf-8") as txt:
             txt.write(
-                f'file \'{SILENCE_FILE_NAME}\'\nfile \'{input_audio_file_path}\''
+                f'file \'{silence_filepath}\'\nfile \'{input_audio_file_path}\''
             )
 
     def ffmpeg_exec(muxed_audio_file_path_concat_formatted: str):
@@ -68,15 +74,21 @@ def delete_concat_txt():
 
 
 def fix_m4a_files(files: list | set, silence_duration: int | float = SILENCE_DURATION):
-    render_silence(silence_duration=silence_duration)
+    silences = dict()
     blank = ''
 
     try:
         for file in files:
-            muxed_filepath = f'{Path(file).with_suffix(blank)}-copy.m4a'
-            concat_silence(file, muxed_filepath)
-
             original_m4a = MP4(file)
+            smpl_rt = original_m4a.info.sample_rate
+
+            # render silence if it does not exist already and add to dict
+            if smpl_rt not in silences.keys():
+                silences[smpl_rt] = render_silence(sample_rate=smpl_rt, silence_duration=silence_duration)
+
+            muxed_filepath = f'{Path(file).with_suffix(blank)}-copy.m4a'
+            concat_silence(file, muxed_filepath, silences[smpl_rt])
+
             muxed_m4a = MP4(muxed_filepath)
             muxed_m4a.tags = original_m4a.tags
             muxed_m4a.save()
@@ -88,7 +100,7 @@ def fix_m4a_files(files: list | set, silence_duration: int | float = SILENCE_DUR
         print(traceback.format_exc())
 
     delete_concat_txt()
-    delete_silence(SILENCE_FILE_NAME)
+    delete_silences(silences)
 
 
 def fix_all_m4a_files_in_root(silence_duration: int | float = SILENCE_DURATION):
